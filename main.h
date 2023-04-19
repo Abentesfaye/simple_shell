819f734a3c5bc83f28f88a60b72c026fd69019e6
“Lunahar21” <kindinewd@gmail.com>
2023-04-19 11:56:34 -0700

    shell_main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include  <unistd.h>
+#include <sys/wait.h>
+#include <errorno.h>
+#define MAX_INPUT_LENGTH 256
+
+#endif
