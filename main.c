937a374166cbf0c880bb4837bebc78e66b3f2f80
Abentesfaye <abentesfaye11@gmail.com>
2023-04-24 01:53:43 +0300

    update main

--- main.c
+++ main.c
@@ -1,62 +1,74 @@
 #include "main.h"
+
 /**
- * @file main.c
- * @brief Simple Shell
- * 
- * Implementation of basic unit command line interpreter that reads user command
- * @main- th execution point of the program
- * Return:nothing
+ * free_data - frees data structure
+ * @datash: data structure
+ * Return: nothing
  * 
  */
-int main()
+
+void free_data(data_shell *datash)
 {
-    char user_input[MAX_INPUT_LENGTH];
-    pid_t pid;
-    int status;
+    unsigned int i;
 
-    while (1)
+    for (i = 0; datash->_environ[i]; i++)
     {
-        /* display prompt */
-        printf("($) ");
-        fflush(stdout);  /* Add fflush(stdout) to flush the output buffer*/
-
-        fgets(user_input, MAX_INPUT_LENGTH, stdin);
-
-        /* check for end of file condition (Ctrl+D) */
-        if (feof(stdin))
-        {
-            break;
-        }
-
-        /*Remove new line character from user input*/
-        user_input[strcspn(user_input, "\n")] = '\0';
-
-        /* Fork a new process */
-        pid = fork();
-
-        if (pid < 0)
-        {
-            perror("Error: Fork failed");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid == 0)
-        {
-            /* child process */
-            if (strcmp(user_input, "") == 0)
-            {
-                /* If user input is empty, exit without executing any command */
-                exit(EXIT_SUCCESS);
-            }
-            execlp(user_input, user_input, (char *)NULL);
-            /* If execlp returns, it means the command was not found */
-            perror("Error: Command not found");
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            /* parent process */
-            wait(&status);
-        }
+        free(datash->_environ[i]);
     }
-    return 0;
+
+    free(datash->_environ);
+    free(datash->pid);
+
+}
+/**
+ * set_data - create the structure
+ * @datash: structute 
+ * @av: argument vector
+ * Return: no return
+ * 
+ */
+void set_data(data_shell *datash, char **av)
+{
+    unsigned int i;
+
+    datash->av = av;
+    datash->input = NULL;
+    datash->status =0;
+    datash->counter = 1;
+
+    for (i = 0; environ[i]; i++)
+        ;
+
+    datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+    for (i = 0; environ[i]; i++)
+    {
+        datash->_environ[i] = stdup(environ[i]);
+    }
+
+    datash->_environ[i] = NULL;
+    datash->pid = aux_itoa(getpid());
+
+}
+
+/**
+ * main - entry point
+ * @ac: argument count
+ * @av: argumet vector
+ * Return: 0
+ * 
+ */
+int main(int ac, char **av)
+{
+    data_shell datash;
+    (void) ac;
+
+    signal(SIGINT, get_sigint);
+    set_data(&datash, av);
+    free_data(&datash);
+    free_data(&datash);
+
+    if (datash.status < 0)
+        return (255);
+    return (datash.status);
 }
