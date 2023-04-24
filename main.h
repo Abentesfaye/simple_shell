5b8b811919da4de951e76d420de180a7cae1b42a
Abentesfaye <abentesfaye11@gmail.com>
2023-04-24 06:06:07 +0300

    update to header

--- main.h
+++ main.h
@@ -99,5 +99,7 @@
  line_list *add_line_node_end(line_list **head, char *line);
  void free_sep_list(sep_list **head);
  sep_list *add_sep_node_end(sep_list **head, char sep);
+ char *without_comment(char *in);
+ void shell_loop(data_shell *datash);
 #endif
 
