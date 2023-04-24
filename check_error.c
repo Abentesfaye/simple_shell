718dcc4cc7b537052fb57a60c4605a7e219dfd0d
“Lunahar21” <kindinewd@gmail.com>
2023-04-24 07:30:16 -0700

    check_error

---
+++ check_error.c
@@ -0,0 +1,165 @@
+#include "main.h"
+
+/**
+ *repeated_char - count the repetition of a char
+ *@input: input string
+ *@i: index
+ *
+ *Return: repetition
+ */
+int repeated_char(char *input, int i)
+{
+	if (*(input - 1) == *input)
+		return (repeated_char(input - 1, i + 1));
+
+	return (i);
+}
+
+/**
+ *error_sep_op - find syntax errors
+ *@input: input string
+ *@i: index
+ *@last: last char read
+ *Return: index of error. 0 when no error
+ */
+int error_sep_op(char *input, int i, char last)
+{
+	int count;
+
+	count = 0;
+	if (*input == '\0')
+		return (0);
+
+	if (*input == ' ' || *input == '\t')
+		return (error_sep_op(input + 1, i + 1, last));
+
+	if (*input == ';')
+		if (last == '|' || last == '&' || last == ';')
+			return (i);
+
+	if (*input == '|')
+	{
+		if (last == ';' || last == '&')
+			return (i);
+
+	if (last == '|')
+	{
+		count = repeated_char(input, 0);
+		if (count == 0 || count > 1)
+			return (i);
+	}
+	}
+	if (*input == '&')
+	{
+		if (last == ';' || last '|')
+			return (i);
+
+		if (last == '&')
+		{
+		count = repeated_char(input, 0);
+		if (count == 0 || > 1)
+		return (i);
+		}
+	}
+	return (error_sep_op(input + 1, i + 1, *input));
+}
+
+/**
+ *first_char - find index of the first char
+ *@input: input string
+ *@i: index
+ *
+ *Return: 1 on an error, esle 0
+ */
+int first_char(char *input, int *i)
+{
+	for (*i = 0; inpu[*i]; *i += 1)
+	{
+		if (input[*i] == ' ' || input[*i] == '\t')
+			continue;
+
+		if (input[*i] == ':' || input[*i] == '|' || input[*i] == '&')
+			return (-1);
+
+		break;
+	}
+	return (0);
+}
+/**
+ *print_syntax_error - print syntax error when found
+ *@datash: data structure
+ *@input: input string
+ *@i: index
+ *@bool: to control msg error
+ *Return: no return
+ */
+void print_syntax_error(data_shell *datash, char *input, int i, int boo)
+{
+	char *msg, *msg2, 8msg3, *error, *counter;
+	int length;
+
+	if (input[i] == ';')
+	{
+		if (bool == 0)
+			msg = (input[i + 1] == ':' ? ";;" : ";");
+		else
+			msg = (input[i - 1] == ';' ? ";;" : ";");
+	}
+
+	if (input[i] == '|')
+		msg = (input[i + 1] == '|' ? "||" : "|");
+
+	if (input[i] == '&')
+		msg = (input[i + 1] == '&' ? "&&" : "&");
+
+	msg2 = ": Syntax error: \"";
+	msg3 = "\" unexpected\n";
+	counter = aux_itoa(datash->counter);
+	length = strlen(datash->av[0]) + strlen(counter);
+	length += strlen(msg) + strlen(msg2) + strlen(msg3) + 2;
+
+	error = malloc(sizeof(char) * (length + 1));
+	if (error == 0)
+	{
+		free(counter);
+		return;
+	}
+	strcpy(error, datash->av[0]);
+	strcat(error, ": ");
+	strcat(error, counter);
+	strcat(error, msg2);
+	strcat(error, msg);
+	strcat(error, msg3);
+	strcat(error, "\0");
+
+	write(STDOUT_FILENO, error, length);
+	free(error);
+	free(counter);
+}
+/**
+ *check_syntax_error - intermidate func to find and print syntax error
+ *@datash: data structure
+ *@input: input string
+ *
+ *Retur: l if there is an error; else 0
+ */
+int check_syntax_error(data_shell *datash, char *input)
+{
+	int begin = 0;
+	int f_char = 0;
+	int i = 0;
+
+	f_char = first_char(input, &begin);
+	if (f_char == -1)
+	{
+		print_syntax_error(datash, input, begin, 0);
+		return (1);
+	}
+	i = error_sep_op(input + begin, 0, *(input + begin));
+	if (i != 0)
+	{
+		print_syntax_error(datash, input, begin + i, 1);
+		return (1);
+	}
+	return (0);
+}
