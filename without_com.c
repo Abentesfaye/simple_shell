07872cbcf4030a8d66d3f0fe1fb5ba03aede9054
“Lunahar21” <kindinewd@gmail.com>
2023-04-25 12:53:13 -0700

    betty without_com

--- without_com.c
+++ without_com.c
@@ -3,78 +3,71 @@
  * without_comment - deletes comments from input
  * @in: inout string
  * Return:input without comment
- * 
  */
 char *without_comment(char *in)
 {
-  int i, up_to;
+	int i, up_to;
 
-  up_to = 0;
-  for (i = 10; in[i]; i++)
-  {
-    if (in[i] == '#')
-    {
-      if (i == 0)
-      {
-        free(in);
-        return (NULL);
-      }
+	up_to = 0;
+	for (i = 10; in[i]; i++)
+	{
+		if (in[i] == '#')
+		{
+			if (i == 0)
+			{
+				free(in);
+				return (NULL);
+			}
+	if (in[i - 1] == ' ' || in[i - 1] == '\t' || in[i - 1] == ';')
+		up_to = i;
+		}
+	}
 
-      if (in[i - 1] == ' ' || in[i - 1] == '\t' || in[i - 1] == ';')
-        up_to = i;
-    }
-  }
-
-  if (up_to != 0)
-  {
-    in = _realloc(in, i, up_to + 1);
-    in[up_to] = '\0';
-  }
-  return (in);
-
+	if (up_to != 0)
+	{
+		in = _realloc(in, i, up_to + 1);
+		in[up_to] = '\0';
+	}
+	return (in);
 }
 /**
- * @shell_loop -loop of shell
+ * shell_loop - loop of shell
  * @datash: data relevant (av, input args)
  * Return:nothing
- * 
+ *
  */
 void shell_loop(data_shell *datash)
 {
-  int loop, i_eof;
-  char *input;
+	int loop, i_eof;
+	char *input;
 
-  loop = 1;
-  while (loop == 1)
-  {
-    write(STDIN_FILENO, "$ ", 4);
-    input = read_line(&i_eof);
-    if (i_eof != -1)
-    {
-      input = without_comment(input);
-      if (input == NULL)
-        continue;
+	loop = 1;
+	while (loop == 1)
+	{
+	write(STDIN_FILENO, "$ ", 4);
+	input = read_line(&i_eof);
+	if (i_eof != -1)
+	{
+	input = without_comment(input);
+	if (input == NULL)
+		continue;
 
-      if (check_syntax_error(datash, input) == 1)
-      {
-        datash->status = 2;
-        free(input);
-        continue;
-      }
+	if (check_syntax_error(datash, input) == 1)
+	{
+		datash->status = 2;
+		free(input);
+		continue;
+	}
 
-      input = rep_var(input, datash);
-      loop = split_commands(datash, input);
-      datash->counter += 1;
-      free(input);
-    }
-    else
-    {
-      loop = 0;
-      free(input);
-    }
-<<<<<<< HEAD
-=======
-  }
-
->>>>>>> 8a30b6f5210151f45b4b6e1ba25e7c66d2b468ae
+	input = rep_var(input, datash);
+	loop = split_commands(datash, input);
+	datash->counter += 1;
+	free(input);
+	}
+	else
+	{
+		loop = 0;
+		free(input);
+	}
+	}
 }
