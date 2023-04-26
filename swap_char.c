6934497643ab9c28f9ecb45e2345b1c8be2365a7
Abentesfaye <abentesfaye11@gmail.com>
2023-04-26 04:13:48 +0300

    hot fixxing

--- swap_char.c
+++ swap_char.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * swap_char - swaps | and & for non-printed chars
  *
@@ -6,42 +7,40 @@
  * @bool: type of swap
  * Return: swapped string
  */
-
 char *swap_char(char *input, int bool)
 {
-    int i;
+	int i;
 
-    if (bool == )
-    {
-        for (i = 0; input[i]; i++)
-        {
-            if (input[i] == '|')
-            {
-                if (input[i + 1] != '|')
-                    input[i] = 16;
-                else
-                    i++;
-            }
+	if (bool == 0)
+	{
+		for (i = 0; input[i]; i++)
+		{
+			if (input[i] == '|')
+			{
+				if (input[i + 1] != '|')
+					input[i] = 16;
+				else
+					i++;
+			}
 
-            if (input[i + 1] = '&')
-            {
-                if (input[i +1] != '&')
-                    input[i] = 12;
-                else
-                    i++;
-    
-            }
-        }
-    }
-    else
-    {
-        for (i = 0; input[i]; i++)
-        {
-            input[i] = (input[i] == 16 ? '|' : input[i]);
-            input[i] = (input[i] == 12 ? '&' : input[i]);
-        }
-    }
-    return (input);
+			if (input[i] == '&')
+			{
+				if (input[i + 1] != '&')
+					input[i] = 12;
+				else
+					i++;
+			}
+		}
+	}
+	else
+	{
+		for (i = 0; input[i]; i++)
+		{
+			input[i] = (input[i] == 16 ? '|' : input[i]);
+			input[i] = (input[i] == 12 ? '&' : input[i]);
+		}
+	}
+	return (input);
 }
 
 /**
