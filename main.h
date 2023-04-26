a225290d5530efb132859d5b8fd7ea98fa618862
Abentesfaye <abentesfaye11@gmail.com>
2023-04-26 03:47:46 +0300

    minor fix

--- main.h
+++ main.h
@@ -151,6 +151,9 @@ int _strlen(const char *s);
 int cmp_chars(char str[], const char *delim);
 char *_strtok(char str[], const char *delim);
 int _isdigit(const char *s);
+void _memcpy(void *newptr, const void *ptr, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
 
